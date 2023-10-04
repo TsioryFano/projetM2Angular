aebe6edc9ae3ed462191366cc60c69c5b1fa2d37
TsioryFano <tsioryfano@hotmail.com>
2023-10-04 16:46:51 +0300

    Ajout gestion erreur Middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,14 +16,33 @@ app.get("/api/sesamiens/byName/:sesmienName", (req, res)=>{
     const sesamienName = req.params.sesmienName;
     const sesamien = SESAMIENS.filter(sesamien => sesamien.name.toLowerCase()
     .includes(sesamienName.toLowerCase()));
-    res.send(sesamien);
-})
+    if (sesamien.length === 0) {
+        // Gérer le cas où aucun résultat n'est trouvé
+        res.status(404).json({ error: "Aucun sesamien trouvé" });
+      } else {
+        res.send(sesamien);
+      }
+});
+    
 
 app.get("/api/sesamiens/byId/:sesmienId", (req, res)=>{
     const sesamienId = req.params.sesmienId;
     const sesamien = SESAMIENS.find(sesamien => sesamien.id == sesamienId);
-    res.send(sesamien);
-})
+    if(!sesamien){
+         // Gérer le cas où aucun résultat n'est trouvé
+        res.status(404).json({ error: "Sesamien non trouvé" });
+    } else {
+        res.send(sesamien);
+    }
+    
+});
+
+// Middleware d'erreur générique
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Erreur interne du serveur" });
+  });
+  
 
 
 const port = 5000;
